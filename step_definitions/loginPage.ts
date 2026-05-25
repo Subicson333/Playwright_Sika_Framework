cb4cbc9791f88aab27701f731ecbeff580b5b3a9
Subicson Manickam <subicson.manickam@qeagle.com>
2026-05-25 18:25:02 +0530

    Add generated tests for execution 906b8187-0edb-4dcc-989b-a9d542951fbb

---
+++ step_definitions/loginPage.ts
@@ -0,0 +1,37 @@
+import { PlaywrightWrapper } from '../utils/wrapper';
+import { createLogger } from '../utils/logger';
+
+const logger = createLogger('login-page-steps');
+
+export class LoginPage {
+  private wrapper: PlaywrightWrapper;
+
+  constructor(wrapper: PlaywrightWrapper) {
+    this.wrapper = wrapper;
+  }
+
+  async navigateTo(): Promise<void> {
+    logger.info('Navigating to login page');
+    await this.wrapper.navigateTo('https://the-internet.herokuapp.com/login');
+  }
+
+  async enterUsername(username: string): Promise<void> {
+    logger.info(`Entering username: ${username}`);
+    await this.wrapper.page.getByLabel('Username').fill(username);
+  }
+
+  async enterPassword(password: string): Promise<void> {
+    logger.info('Entering password');
+    await this.wrapper.page.getByLabel('Password').fill(password);
+  }
+
+  async clickLoginButton(): Promise<void> {
+    logger.info('Clicking login button');
+    await this.wrapper.page.getByRole('button', { name: 'Login' }).click();
+  }
+
+  async isSecureAreaVisible(): Promise<boolean> {
+    logger.info('Checking if secure area is visible');
+    return await this.wrapper.page.getByText('Secure Area').isVisible();
+  }
+}
---
+++ step_definitions/loginSteps.ts
@@ -0,0 +1,34 @@
+import { When, Then } from '@cucumber/cucumber';
+import { PlaywrightWrapper } from '../utils/wrapper';
+import { createLogger } from '../utils/logger';
+import { LoginPage } from './loginPage';
+
+const logger = createLogger('login-steps');
+
+let loginPage: LoginPage;
+
+When('I navigate to {string}', async (url: string) => {
+  loginPage = new LoginPage(new PlaywrightWrapper(this.page, this.context));
+  await loginPage.navigateTo();
+});
+
+When('I enter {string} in the {string} field', async (value: string, field: string) => {
+  if (field === 'Username') {
+    await loginPage.enterUsername(value);
+  } else if (field === 'Password') {
+    await loginPage.enterPassword(value);
+  }
+});
+
+When('I click the {string} button', async (button: string) => {
+  if (button === 'Login') {
+    await loginPage.clickLoginButton();
+  }
+});
+
+Then('I should see {string}', async (text: string) => {
+  if (text === 'Secure Area') {
+    const isVisible = await loginPage.isSecureAreaVisible();
+    expect(isVisible).toBe(true);
+  }
+});
