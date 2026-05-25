24b4851714e557fedf96299c36635a0bb349343f
Subicson Manickam <subicson.manickam@qeagle.com>
2026-05-25 19:23:53 +0530

    Add generated tests for execution 74e60f2c-a07b-4f78-aa9d-6d91c260905c

---
+++ step_definitions/loginSteps.ts
@@ -0,0 +1,33 @@
+import { When, Then } from '@cucumber/cucumber';
+import { PlaywrightWrapper } from '../utils/wrapper';
+import { createLogger } from '../utils/logger';
+import { LoginPage } from './loginPage';
+
+const logger = createLogger('login-steps');
+
+let loginPage: LoginPage;
+
+When('I navigate to {string}', async (url: string) => {
+  loginPage = new LoginPage(new PlaywrightWrapper(this.world.page, this.world.context));
+  await loginPage.navigateTo();
+});
+
+When('I enter {string} in the {string} field', async (value: string, field: string) => {
+  if (field === 'Username') {
+    await loginPage.enterUsername(value);
+  } else if (field === 'Password') {
+    await loginPage.enterPassword(value);
+  }
+});
+
+When('I click the {string} button', async (button: string) => {
+  if (button === 'Login') {
+    await loginPage.clickLoginButton();
+  }
+});
+
+Then('I should see {string}', async (text: string) => {
+  if (text === 'Secure Area') {
+    expect(await loginPage.isSecureAreaVisible()).toBe(true);
+  }
+});
